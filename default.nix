3ad2b8fec870fdbe0ef9dc566c4d03bf5bd587d3
lucas8 <luc.linux@mailoo.org>
2017-11-07 08:49:33 +0100

    Add nix builder

---
+++ default.nix
@@ -0,0 +1,9 @@
+{ nixpkgs ? import <nixpkgs> {} }:
+let
+  inherit (nixpkgs) pkgs;
+  packages = with nixpkgs.rustChannels.stable; [ rust ];
+in pkgs.stdenv.mkDerivation {
+  name = "programmation-parallele-tp1";
+  buildInputs =  packages;
+}
+
