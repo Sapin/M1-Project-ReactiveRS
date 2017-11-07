1aba0b56e5556d319167b39fb3e4b69cf76269dd
David Reboullet <david.reboullet@ens.fr>
2017-11-07 09:50:56 +0100

    Add and implement the trait Map

--- src/runtime.rs
+++ src/runtime.rs
@@ -1,6 +1,10 @@
 use std::collections::VecDeque;
 use std::mem;
 
+	//////////////////
+	// CONTINUATION //
+	//////////////////
+
 /// A reactive continuation awaiting a value of type `V`. For the sake of
 /// simplicity, continuation must be valid on the static lifetime.
 pub trait Continuation<V>: 'static {
@@ -13,6 +17,13 @@ pub trait Continuation<V>: 'static {
 	/// It is thus impossible to take the ownership of a `Box<Continuation>`
 	/// whitout knowing the underlying type of the `Continuation`.
 	fn call_box(self: Box<Self>, runtime: &mut Runtime, value: V);
+
+	/// Creates a new continuation that applies a function to the input value
+	/// before calling `Self`.
+	fn map<F, V2>(self, map: F) -> Map<Self, F>
+	where Self: Sized, F: FnOnce(V2) -> V + 'static {
+		Map { continuation: self, map }
+	}
 }
 
 impl<V, F> Continuation<V> for F where F: FnOnce(&mut Runtime, V) + 'static {
@@ -24,6 +35,32 @@ impl<V, F> Continuation<V> for F where F: FnOnce(&mut Runtime, V) + 'static {
 		(*self).call(runtime, value);
 	}
 }
+
+	/////////
+	// MAP //
+	/////////
+
+/// A continuation that applies a function before calling another continuation.
+pub struct Map<C, F> {
+	continuation: C,
+	map: F
+}
+
+impl<C, F, V1, V2> Continuation<V1> for Map<C, F>
+    where C: Continuation<V2>, F: FnOnce(V1) -> V2 + 'static
+{
+	fn call (self, runtime: &mut Runtime, value: V1) {
+		self.continuation.call (runtime, (self.map) (value))
+	}
+
+	fn call_box (self : Box<Self>, runtime: &mut Runtime, value: V1) {
+		(*self).call (runtime, value)
+	}
+}
+
+	/////////////
+	// RUNTIME //
+	/////////////
 
 /// Runtime for executing reactive continuations.
 pub struct Runtime {
